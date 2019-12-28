38fd238c340fea46d020782afcbed247f8813247
Magno Valdetaro <mvaldetaro@gmail.com>
2019-12-28 19:22:35 -0300

    Create main.spec.js

---
+++ tests/main.spec.js
@@ -0,0 +1,19 @@
+describe('Main', () => {
+  describe('Soma', () => {
+    context('Caso 1'),
+      () => {
+        it('Deveria somar 2 + 2 e retornar 4', () => {
+          // esperado a acontecer
+        });
+      };
+  });
+
+  describe('Subtração', () => {
+    context('Caso 1'),
+      () => {
+        it('Deveria subtrair 2 - 2 e retornar 0', () => {
+          // esperado a acontecer
+        });
+      };
+  });
+});
