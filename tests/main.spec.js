c20ef5f9d7e5f8bb259ba4d6430967eb9435830a
Magno Valdetaro <mvaldetaro@gmail.com>
2019-12-28 20:01:10 -0300

    skip e only

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,16 +1,18 @@
 describe('Main', () => {
   describe('Soma', () => {
-    context('Caso 1', () => {
+    context.only('Caso 1', () => {
       it('Deveria somar 2 + 2 e retornar 4', () => {
         // esperado a acontecer
+        throw new Error('Somente um erro');
       });
     });
   });
 
   describe('Subtração', () => {
-    context('Caso 2', () => {
+    context.skip('Caso 2', () => {
       it('Deveria subtrair 2 - 2 e retornar 0', () => {
         // esperado a acontecer
+        throw new Error('Somente um erro');
       });
     });
   });
