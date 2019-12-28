8d5b7a87a4e45cc2eaab3d279c1db75291f4dc7c
Magno Valdetaro <mvaldetaro@gmail.com>
2019-12-28 19:35:39 -0300

    config mocha

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   extends: 'airbnb-base',
-  plugins: ['import', 'mocha']
+  plugins: ['import', 'mocha'],
+  env: {
+    commonjs: true,
+    node: true,
+    mocha: true
+  }
 };
