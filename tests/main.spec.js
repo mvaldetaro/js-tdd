cab9e603392034c9975a359ae2e99644a2ca8169
Magno Valdetaro <mvaldetaro@gmail.com>
2019-12-28 19:36:09 -0300

    Estrutura básica de teste

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,19 +1,17 @@
 describe('Main', () => {
   describe('Soma', () => {
-    context('Caso 1'),
-      () => {
-        it('Deveria somar 2 + 2 e retornar 4', () => {
-          // esperado a acontecer
-        });
-      };
+    context('Caso 1', () => {
+      it('Deveria somar 2 + 2 e retornar 4', () => {
+        // esperado a acontecer
+      });
+    });
   });
 
   describe('Subtração', () => {
-    context('Caso 1'),
-      () => {
-        it('Deveria subtrair 2 - 2 e retornar 0', () => {
-          // esperado a acontecer
-        });
-      };
+    context('Caso 2', () => {
+      it('Deveria subtrair 2 - 2 e retornar 0', () => {
+        // esperado a acontecer
+      });
+    });
   });
 });
