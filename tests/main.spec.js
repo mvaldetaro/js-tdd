32f4f94efd9b5c926a97a123e77a003f9073bb78
Magno Valdetaro <mvaldetaro@gmail.com>
2019-12-28 23:03:26 -0300

    Update main.spec.js

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,4 +1,4 @@
-const expect = require('chai').expect;
+const { expect } = require('chai');
 
 describe('Main', () => {
   let arr;
@@ -43,6 +43,6 @@ describe('Main', () => {
   });
 
   it('deve retornar true quando pop igual a 3', () => {
-    expect(arr.pop() === 3).to.be.true;
+    expect(arr.pop()).to.equal(3);
   });
 });
