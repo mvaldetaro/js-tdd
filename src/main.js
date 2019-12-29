0d006ad3e1b2b5867c21afebbf778020badca128
Magno Valdetaro <mvaldetaro@gmail.com>
2019-12-29 11:26:14 -0300

    Add check-coverage

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const FizzBuzz = (pNum) => {
+const FizzBuzz = pNum => {
   if (pNum === 0) return 0;
 
   if (pNum % 3 === 0 && pNum % 5 === 0) {
@@ -15,5 +15,7 @@ const FizzBuzz = (pNum) => {
 
   return pNum;
 };
+
+const foo = () => 'bla';
 
 export default FizzBuzz;
