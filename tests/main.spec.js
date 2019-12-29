234d8f375f9bb74b5d979451e3d423fea5937fef
Magno Valdetaro <mvaldetaro@gmail.com>
2019-12-29 15:26:28 -0300

    limpeza

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -8,28 +8,7 @@ Se não for múltiplo de nada, retorna o número
  */
 
 import { expect } from 'chai';
-import FizzBuzz from '../src/main';
 
-describe('FizzBuzz', () => {
-  it('deve retornar `Fizz` quando multiplo de 3', () => {
-    expect(FizzBuzz(3)).to.be.equal('Fizz');
-    expect(FizzBuzz(6)).to.be.equal('Fizz');
-  });
+describe('Spotify', () => {
 
-  it('deve retornar `Buzz` quando multiplo de 5', () => {
-    expect(FizzBuzz(5)).to.be.equal('Buzz');
-    expect(FizzBuzz(10)).to.be.equal('Buzz');
-  });
-
-  it('deve retornar `FizzBuzz` quando multiplo de 3 e 5', () => {
-    expect(FizzBuzz(15)).to.be.equal('FizzBuzz');
-  });
-
-  it('deve retornar o valor quando não for multiplo de 3 ou 5', () => {
-    expect(FizzBuzz(2)).to.be.equal(2);
-  });
-
-  it('deve retornar 0 quando o valor for 0', () => {
-    expect(FizzBuzz(0)).to.be.equal(0);
-  });
 });
