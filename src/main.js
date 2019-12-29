23eb077441ef22955f151d26e586aa6bd3263b12
Magno Valdetaro <mvaldetaro@gmail.com>
2019-12-29 16:40:36 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 const BASE_URL = 'https://api.spotify.com/v1';
 
-export default const search = (pQuery, pType) => {
+const search = (pQuery, pType) => {
   fetch(`${BASE_URL}/search?q=${pQuery}&type=${pType}`);
 };
 // export const searchAlbuns = () => {};
 // export const searchArtists = () => {};
 // export const searchTracks = () => {};
 // export const searchPlaylists = () => {};
+
+export default search;
