e25ca67eef1ec1948bee02760ff2816beb874174
Magno Valdetaro <mvaldetaro@gmail.com>
2019-12-28 21:42:06 -0300

    hooks

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,19 +1,47 @@
 describe('Main', () => {
-  describe('Soma', () => {
-    context.only('Caso 1', () => {
-      it('Deveria somar 2 + 2 e retornar 4', () => {
-        // esperado a acontecer
-        throw new Error('Somente um erro');
-      });
-    });
+  // roda uma vez, antes do bloco
+  before(() => {
+    console.log('before');
   });
 
-  describe('Subtração', () => {
-    context.skip('Caso 2', () => {
-      it('Deveria subtrair 2 - 2 e retornar 0', () => {
-        // esperado a acontecer
-        throw new Error('Somente um erro');
-      });
-    });
+  // roda uma vez, depois do bloco
+  after(() => {
+    console.log('after');
   });
+
+  // roda todas as vezes antes de cada bloco
+  beforeEach(() => {
+    console.log('beforeEach');
+  });
+
+  // roda todas as vezes depois de cada bloco
+  afterEach(() => {
+    console.log('afterEach');
+  });
+
+  it('test 1', () => {
+    console.log('Teste 1');
+  });
+
+  it('test 2', () => {
+    console.log('Teste 2');
+  });
+
+  // describe('Soma', () => {
+  //   context('Caso 1', () => {
+  //     it('Deveria somar 2 + 2 e retornar 4', () => {
+  //       // esperado a acontecer
+  //       throw new Error('Somente um erro');
+  //     });
+  //   });
+  // });
+
+  // describe('Subtração', () => {
+  //   context('Caso 2', () => {
+  //     it('Deveria subtrair 2 - 2 e retornar 0', () => {
+  //       // esperado a acontecer
+  //       throw new Error('Somente um erro');
+  //     });
+  //   });
+  // });
 });
