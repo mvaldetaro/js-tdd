a5cd6976ebdb6925c926522f05ef1294a04eaaf5
Magno Valdetaro <mvaldetaro@gmail.com>
2019-12-29 15:41:58 -0300

    smoke tests

---
+++ src/main.js
@@ -0,0 +1,5 @@
+export const search = () => {};
+export const searchAlbuns = () => {};
+export const searchArtists = () => {};
+export const searchTracks = () => {};
+export const searchPlaylists = () => {};
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,14 +1,33 @@
 /*
-Desafio FizzBuzz
-Escreva uma lib que receba um número e:
-Se o número for divisível por 3, no lugar do número escreva 'Fizz'
-Se o número for divisível por 5, no lugar do número escreva 'Buzz'
-Se o número for divisível por 3 e 5, no lugar do número escreva 'FizzBuzz'
-Se não for múltiplo de nada, retorna o número
- */
+Spotify Wrapper API
+*/
 
 import { expect } from 'chai';
+import {
+  search, searchAlbuns, searchArtists, searchTracks, searchPlaylists,
+} from '../src/main';
 
-describe('Spotify', () => {
-
+describe('Spotify Wrapper', () => {
+  describe('smoke tests', () => {
+    // search (gen)
+    it('deve existir o metodo search', () => {
+      expect(search).to.exist;
+    });
+    // searchAlbuns
+    it('deve existir o metodo searchAlbuns', () => {
+      expect(searchAlbuns).to.exist;
+    });
+    // searchArtists
+    it('deve existir o metodo searchArtists', () => {
+      expect(searchArtists).to.exist;
+    });
+    // searchTracks
+    it('deve existir o metodo searchTracks', () => {
+      expect(searchTracks).to.exist;
+    });
+    // searchPlaylists
+    it('deve existir o metodo searchPlaylists', () => {
+      expect(searchPlaylists).to.exist;
+    });
+  });
 });
