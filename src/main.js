bf9a69a59270759b6a189ee1245699e57f3ecbc8
Magno Valdetaro <mvaldetaro@gmail.com>
2019-12-29 10:50:13 -0300

    Desafio FizzBuzz

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
+const FizzBuzz = (pNum) => {
+  if (pNum % 3 === 0 && pNum % 5 === 0) {
+    return 'FizzBuzz';
+  }
 
+  if (pNum % 3 === 0) {
+    return 'Fizz';
+  }
+
+  if (pNum % 5 === 0) {
+    return 'Buzz';
+  }
+
+  return pNum;
+};
+
+export default FizzBuzz;
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,1 +1,28 @@
+/*
+Desafio FizzBuzz
+Escreva uma lib que receba um número e:
+Se o número for divisível por 3, no lugar do número escreva 'Fizz'
+Se o número for divisível por 5, no lugar do número escreva 'Buzz'
+Se o número for divisível por 3 e 5, no lugar do número escreva 'FizzBuzz'
+Se não for múltiplo de nada, retorna o número
+ */
+
 import { expect } from 'chai';
+import FizzBuzz from '../src/main';
+
+describe('FizzBuzz', () => {
+  it('deve retornar `Fizz` quando multiplo de 3', () => {
+    expect(FizzBuzz(3)).to.be.equal('Fizz');
+    expect(FizzBuzz(6)).to.be.equal('Fizz');
+  });
+  it('deve retornar `Buzz` quando multiplo de 5', () => {
+    expect(FizzBuzz(5)).to.be.equal('Buzz');
+    expect(FizzBuzz(10)).to.be.equal('Buzz');
+  });
+  it('deve retornar `FizzBuzz` quando multiplo de 3 e 5', () => {
+    expect(FizzBuzz(15)).to.be.equal('FizzBuzz');
+  });
+  it('deve retornar quando não for multiplo de 3 ou 5', () => {
+    expect(FizzBuzz(2)).to.be.equal(2);
+  });
+});
