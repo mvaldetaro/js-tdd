cf0e4e37d2b9073567600f11a81cb6ebd474e914
Magno Valdetaro <mvaldetaro@gmail.com>
2019-12-29 10:27:59 -0300

    Commit inicial

--- src/main.js
+++ src/main.js
@@ -1,9 +1,1 @@
-// calc
-const sum = (pN1, pN2) => pN1 + pN2;
-const sub = (pN1, pN2) => pN1 - pN2;
-const mult = (pN1, pN2) => pN1 * pN2;
-const div = (pN1, pN2) => ((pN2 === 0) ? 'Não é possível dividir por zero!' : pN1 / pN2);
 
-export {
-  sum, sub, mult, div,
-};
