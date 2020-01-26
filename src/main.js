ce32cfa5cf21be2c411d3b4d3d6b1fd9fe4cb913
Magno Valdetaro <mvaldetaro@gmail.com>
2020-01-26 10:03:05 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,15 @@
 const BASE_URL = 'https://api.spotify.com/v1';
 
-export const search = (pQuery, pType) => fetch(`${BASE_URL}/search?q=${pQuery}&type=${pType}`).then(rData => {
-  const xJsonData = rData.json();
-  return xJsonData;
-});
-export const searchAlbuns = (pQuery) => {
-  search(pQuery, 'album');
-};
-export const searchArtists = (pQuery) => search(pQuery, 'artist');
-export const searchTracks = (pQuery) => {
+export const search = (pQuery, pType) =>
+  fetch(`${BASE_URL}/search?q=${pQuery}&type=${pType}`).then(rData => {
+    const xJsonData = rData.json();
+    return xJsonData;
+  });
+export const searchAlbuns = pQuery => search(pQuery, 'album');
+export const searchArtists = pQuery => search(pQuery, 'artist');
+export const searchTracks = pQuery => {
   search(pQuery, 'track');
 };
-export const searchPlaylists = (pQuery) => {
+export const searchPlaylists = pQuery => {
   search(pQuery, 'playlist');
 };
